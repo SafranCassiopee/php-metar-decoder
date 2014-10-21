0bbdc2dfe74a1a7ac8ee1fc2cdce9b346952e817
inouire <edouard@melix.net>
2014-10-21 22:25:34 +0200

    Simpler manual loading

--- src/MetarDecoder/MetarDecoder.inc.php
+++ src/MetarDecoder/MetarDecoder.inc.php
@@ -1,17 +1,26 @@
 <?php
 
-# Use this file if you cannot use class autoloading. It will include all
-# the files needed for the Metar decoder.
-#
-# Use composer to install this library if you want a simple autoloader setup.
-# To know how to use composer, see README.md
+/**
+ * Use this file if you cannot use class autoloading. It will include all
+ * the files needed for the Metar decoder.
+ *
+ * Use composer to install this library if you want a simple autoloader setup.
+ * To know how to use composer, see README.md
+ */
 
-require_once dirname(__FILE__) . '/MetarDecoder.php';
-require_once dirname(__FILE__) . '/Entity/DecodedMetar.php';
+$to_include = array(
+    'MetarDecoder.php',
+    'Entity/DecodedMetar.php',
+    
+    'Service/MetarChunkDecoder.php',
+    'Service/MetarChunkDecoderInterface.php',
+    
+    'Service/ReportTypeChunkDecoder.php',
+    'Service/IcaoChunkDecoder.php',
+    'Service/DatetimeChunkDecoder.php'
+);
 
-require_once dirname(__FILE__) . '/Service/MetarChunkDecoder.php';
-require_once dirname(__FILE__) . '/Service/MetarChunkDecoderInterface.php';
+foreach($to_include as $file){
+    require_once dirname(__FILE__) . '/'.$file;
+}
 
-require_once dirname(__FILE__) . '/Service/ReportTypeChunkDecoder.php';
-require_once dirname(__FILE__) . '/Service/IcaoChunkDecoder.php';
-require_once dirname(__FILE__) . '/Service/DatetimeChunkDecoder.php';
