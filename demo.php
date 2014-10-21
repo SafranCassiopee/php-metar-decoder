7332f21f8a0a8eafcfdece936f58ef32389c0d78
inouire <edouard@melix.net>
2014-10-21 22:37:00 +0200

    Add demo script to show how to use the library for real

---
+++ demo.php
@@ -0,0 +1,21 @@
+<?php
+
+// This is a live demonstration on how to use the library
+
+require_once dirname(__FILE__) . '/vendor/autoload.php';
+// or if you don't use autoloading:
+// require_once dirname(__FILE__) . '/src/MetarDecoder/MetarDecoder.inc.php';
+
+use MetarDecoder\MetarDecoder;
+
+$raw_metar = 'METAR LFPG 212205Z';
+
+print('Demo, decoding raw metar "'.$raw_metar.'"'.PHP_EOL);
+
+$decoder = new MetarDecoder();
+$decoded_metar = $decoder->parse($raw_metar);
+
+print('Report type: '.$decoded_metar->getType().PHP_EOL);
+print('ICAO: '.$decoded_metar->getIcao().PHP_EOL);
+print('Day: '.$decoded_metar->getDay().PHP_EOL);
+print('Time: '.$decoded_metar->getTime()->format('H:i').' UTC'.PHP_EOL);
