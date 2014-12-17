931d8591fc1a22b3308b5187eb8731bf0cca4289
Edouard <edouard@melix.net>
2014-12-17 16:50:24 +0100

    Add chunk decoder for clouds section

---
+++ src/ChunkDecoder/CloudChunkDecoder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace MetarDecoder\ChunkDecoder;
+
+use MetarDecoder\Exception\ChunkDecoderException;
+use MetarDecoder\Entity\CloudLayer;
+
+/**
+ * Chunk decoder for cloud section
+ */
+class CloudChunkDecoder extends MetarChunkDecoder implements MetarChunkDecoderInterface
+{
+    public function getRegexp()
+    {
+        $no_cloud = "(NSC|NCD|CLR|SKC)";
+        $layer = "(FEW|SCT|BKN|OVC|///)([0-9]{3}|///)(CB|TCU|///)?";
+        $vertical_visibility = "VV([0-9]{3}|///)";
+        
+        return "#^($no_cloud|($layer)( $layer)?( $layer)?( $layer)?( $vertical_visibility)?)( )#";
+    }
+
+    public function parse($remaining_metar)
+    {
+        $found = $this->applyRegexp($remaining_metar);
+        var_dump($found);
+        // handle the case where nothing has been found
+        if ($found == null) {
+            // TODO no error in the case of CAVOK
+            throw new ChunkDecoderException($remaining_metar, 'Bad format for clouds information, applied regexp is "'.$this->getRegexp().'"', $this);
+        }
+
+        if($found[2] != null){
+            // handle the case where no clouds observed
+            $layers = null;
+            $visibility = null;
+            // TODO what fields ?
+        }else{
+            // handle cloud layers and visibility
+            $layers = array();
+            for($i = 3; $i <= 15 ; $i+= 4 ){
+                if($found[$i] != null){
+                    $layer = new CloudLayer();
+                    $layer->setAmount($found[$i+1])
+                          ->setBaseHeight($found[$i+2])
+                          ->setType($found[$i+3]);
+                    $layers[] = $layer;
+                }
+            }
+            if($found[19] != null){
+                $visibility = $found[20];
+            }
+        }
+        
+        // return result + remaining metar
+        return array(
+            'result' => array(
+                'clouds' => $layers,
+                'verticalVisibility' => $visibility,
+            ),
+            'remaining_metar' => $this->getRemainingMetar($remaining_metar),
+        );
+        
+
+    }
+}
---
+++ src/Entity/CloudLayer.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace MetarDecoder\Entity;
+
+class CloudLayer
+{
+    // annotation corresponding to amount of clouds (FEW/SCT/BKN/OVC)
+    private $amount;
+    
+    // height of cloud base, unit ?
+    private $base_height;
+    
+    // cloud type cumulonimbus, towering cumulonimbus (CB/TCU)
+    private $type;
+
+    public function __construct()
+    {
+        $this->amount = null;
+        $this->base_height = null;
+        $this->type = null;
+    }
+    
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+    
+    public function getBaseHeight()
+    {
+        return $this->base_height;
+    }
+
+    public function setBaseHeight($base_height)
+    {
+        $this->base_height = $base_height;
+
+        return $this;
+    }
+    
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+    
+}
