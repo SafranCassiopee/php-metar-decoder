4a4786455312cf79bf1da9f5e5a2e1884cae48b4
dennis <a>
2019-10-03 23:02:58 +0200

    fixes #25

--- src/ChunkDecoder/VisibilityChunkDecoder.php
+++ src/ChunkDecoder/VisibilityChunkDecoder.php
@@ -11,66 +11,69 @@ use MetarDecoder\Entity\Value;
  */
 class VisibilityChunkDecoder extends MetarChunkDecoder implements MetarChunkDecoderInterface
 {
-    public function getRegexp()
-    {
-        $cavok = 'CAVOK';
-        $visibility = '([0-9]{4})(NDV)?';
-        $us_visibility = 'M?([0-9]{0,2}) ?(([1357])/(2|4|8|16))?SM';
-        $minimum_visibility = '( ([0-9]{4})(N|NE|E|SE|S|SW|W|NW)?)?';// optional
-        $no_info = '////';
+	public function getRegexp()
+	{
+		$cavok = 'CAVOK';
+		$visibility = '([0-9]{4})(NDV)?';
+		$us_visibility = 'M?([0-9]{0,2}) ?(([1357])/(2|4|8|16))?SM';
+		$minimum_visibility = '( ([0-9]{4})(N|NE|E|SE|S|SW|W|NW)?)?';// optional
+		$km_visibility = '([0-9][05])(KM)?(NDV)?';
+		$no_info = '////';
 
-        return "#^($cavok|$visibility$minimum_visibility|$us_visibility|$no_info)( )#";
-    }
+		return "#^($cavok|$visibility$minimum_visibility|$us_visibility|$km_visibility|$no_info)( )#";
+	}
 
-    public function parse($remaining_metar, $cavok = false)
-    {
-        $result = $this->consume($remaining_metar);
-        $found = $result['found'];
-        $new_remaining_metar = $result['remaining'];
+	public function parse($remaining_metar, $cavok = false)
+	{
+		$result = $this->consume($remaining_metar);
+		$found = $result['found'];
+		$new_remaining_metar = $result['remaining'];
+		echo print_r($found);
+		// handle the case where nothing has been found
+		if ($found == null) {
+			throw new ChunkDecoderException($remaining_metar,
+				$new_remaining_metar,
+				'Bad format for visibility information',
+				$this);
+		}
+		if ($found[1] ==  'CAVOK') { // cloud and visibility OK
+			$cavok = true;
+			$visibility = null;
+		} elseif ($found[1] == '////') { // information not available
+			$cavok = false;
+			$visibility = null;
+		} else {
+			$cavok = false;
+			$visibility = new Visibility();
+			if ($found[2] != null) { // icao visibility
+				$visibility->setVisibility(Value::newIntValue($found[2], Value::METER));
+				if ($found[4] != null) {
+					$visibility->setMinimumVisibility(Value::newIntValue($found[5], Value::METER))
+					->setMinimumVisibilityDirection($found[6]);
+				}
+				$visibility->setNDV($found[3] != null);
+			} else if ($found[11] != null) { // km visibility
+				$visibility->setVisibility(Value::newIntValue($found[11] * 1000, Value::METER));
+			} else { // us visibility
+				$main = intval($found[7]);
+				$frac_top = intval($found[9]);
+				$frac_bot = intval($found[10]);
+				if ($frac_bot != 0) {
+					$vis_value = $main + $frac_top / $frac_bot;
+				} else {
+					$vis_value = $main;
+				}
+				$visibility->setVisibility(Value::newValue($vis_value, Value::STATUTE_MILE));
+			}
+		}
 
-        // handle the case where nothing has been found
-        if ($found == null) {
-            throw new ChunkDecoderException($remaining_metar,
-                                            $new_remaining_metar,
-                                            'Bad format for visibility information',
-                                            $this);
-        }
-        if ($found[1] ==  'CAVOK') { // cloud and visibility OK
-            $cavok = true;
-            $visibility = null;
-        } elseif ($found[1] == '////') { // information not available
-            $cavok = false;
-            $visibility = null;
-        } else {
-            $cavok = false;
-            $visibility = new Visibility();
-            if ($found[2] != null) { // icao visibility
-                $visibility->setVisibility(Value::newIntValue($found[2], Value::METER));
-                if ($found[4] != null) {
-                    $visibility->setMinimumVisibility(Value::newIntValue($found[5], Value::METER))
-                                ->setMinimumVisibilityDirection($found[6]);
-                }
-                $visibility->setNDV($found[3] != null);
-            } else { // us visibility
-                $main = intval($found[7]);
-                $frac_top = intval($found[9]);
-                $frac_bot = intval($found[10]);
-                if ($frac_bot != 0) {
-                    $vis_value = $main + $frac_top / $frac_bot;
-                } else {
-                    $vis_value = $main;
-                }
-                $visibility->setVisibility(Value::newValue($vis_value, Value::STATUTE_MILE));
-            }
-        }
-
-        // return result + remaining metar
-        return array(
-            'result' => array(
-                'cavok' => $cavok,
-                'visibility' => $visibility,
-            ),
-            'remaining_metar' => $new_remaining_metar,
-        );
-    }
+		// return result + remaining metar
+		return array(
+			'result' => array(
+				'cavok' => $cavok,
+				'visibility' => $visibility,
+			),
+			'remaining_metar' => $new_remaining_metar,
+		);
+	}
 }
--- tests/ChunkDecoder/VisibilityChunkDecoderTest.php
+++ tests/ChunkDecoder/VisibilityChunkDecoderTest.php
@@ -6,162 +6,183 @@ use MetarDecoder\ChunkDecoder\VisibilityChunkDecoder;
 
 class VisibilityChunkDecoderTest extends \PHPUnit_Framework_TestCase
 {
-    private $decoder;
+	private $decoder;
 
-    protected function setup()
-    {
-        $this->decoder = new VisibilityChunkDecoder();
-    }
+	protected function setup()
+	{
+		$this->decoder = new VisibilityChunkDecoder();
+	}
 
-    /**
-     * Test parsing of valid surface wind chunks.
-     *
-     * @param $chunk
-     * @param $cavok
-     * @param $visibility
-     * @param $visibility_unit
-     * @param $minimum
-     * @param $minimum_direction
-     * @param $remaining
-     * @dataProvider getChunk
-     */
-    public function testParse($chunk, $cavok, $visibility, $visibility_unit, $minimum, $minimum_direction, $has_ndv, $remaining)
-    {
-        $decoded = $this->decoder->parse($chunk);
-        if ($cavok) {
-            $this->assertTrue($decoded['result']['cavok']);
-        } elseif ($visibility == null) {
-            $this->assertNull($decoded['result']['visibility']);
-            $this->assertFalse($decoded['result']['cavok']);
-        } else {
-            $vis = $decoded['result']['visibility'];
-            $this->assertEquals($visibility, $vis->getVisibility()->getValue());
-            $this->assertEquals($visibility_unit, $vis->getVisibility()->getUnit());
-            $this->assertEquals($has_ndv, $vis->hasNDV());
-            if ($minimum != null) {
-                $this->assertEquals($minimum, $vis->getMinimumVisibility()->getValue());
-                $this->assertEquals($minimum_direction, $vis->getMinimumVisibilityDirection());
-            }
-        }
-        $this->assertEquals($remaining, $decoded['remaining_metar']);
-    }
+	/**
+	 * Test parsing of valid surface wind chunks.
+	 *
+	 * @param $chunk
+	 * @param $cavok
+	 * @param $visibility
+	 * @param $visibility_unit
+	 * @param $minimum
+	 * @param $minimum_direction
+	 * @param $remaining
+	 * @dataProvider getChunk
+	 */
+	public function testParse($chunk, $cavok, $visibility, $visibility_unit, $minimum, $minimum_direction, $has_ndv, $remaining)
+	{
+		$decoded = $this->decoder->parse($chunk);
+		if ($cavok) {
+			$this->assertTrue($decoded['result']['cavok']);
+		} elseif ($visibility == null) {
+			$this->assertNull($decoded['result']['visibility']);
+			$this->assertFalse($decoded['result']['cavok']);
+		} else {
+			$vis = $decoded['result']['visibility'];
+			$this->assertEquals($visibility, $vis->getVisibility()->getValue());
+			$this->assertEquals($visibility_unit, $vis->getVisibility()->getUnit());
+			$this->assertEquals($has_ndv, $vis->hasNDV());
+			if ($minimum != null) {
+				$this->assertEquals($minimum, $vis->getMinimumVisibility()->getValue());
+				$this->assertEquals($minimum_direction, $vis->getMinimumVisibilityDirection());
+			}
+		}
+		$this->assertEquals($remaining, $decoded['remaining_metar']);
+	}
 
-    /**
-     * Test parsing of invalid surface wind chunks.
-     *
-     * @param $chunk
-     * @expectedException \MetarDecoder\Exception\ChunkDecoderException
-     * @dataProvider getInvalidChunk
-     */
-    public function testParseInvalidChunk($chunk)
-    {
-        $this->decoder->parse($chunk);
-    }
+	/**
+	 * Test parsing of invalid surface wind chunks.
+	 *
+	 * @param $chunk
+	 * @expectedException \MetarDecoder\Exception\ChunkDecoderException
+	 * @dataProvider getInvalidChunk
+	 */
+	public function testParseInvalidChunk($chunk)
+	{
+		$this->decoder->parse($chunk);
+	}
 
-    public function getChunk()
-    {
-        return array(
-            array(
-                'chunk' => '0200 AAA',
-                'cavok' => false,
-                'visibility' => 200,
-                'visibility_unit' => 'm',
-                'minimum' => null,
-                'minimum_direction' => null,
-                'ndv' => false,
-                'remaining' => 'AAA',
-            ),
-            array(
-                'chunk' => 'CAVOK BBB',
-                'cavok' => true,
-                'visibility' => null,
-                'visibility_unit' => 'm',
-                'minimum' => null,
-                'minimum_direction' => null,
-                'ndv' => false,
-                'remaining' => 'BBB',
-            ),
-            array(
-                'chunk' => '8000 1200N CCC',
-                'cavok' => false,
-                'visibility' => 8000,
-                'visibility_unit' => 'm',
-                'minimum' => 1200,
-                'minimum_direction' => 'N',
-                'ndv' => false,
-                'remaining' => 'CCC',
-            ),
-            array(
-                'chunk' => '2500 2200 DDD',
-                'cavok' => false,
-                'visibility' => 2500,
-                'visibility_unit' => 'm',
-                'minimum' => 2200,
-                'minimum_direction' => null,
-                'ndv' => false,
-                'remaining' => 'DDD',
-            ),
-            array(
-                'chunk' => '1 1/4SM EEE',
-                'cavok' => false,
-                'visibility' => 1.25,
-                'visibility_unit' => 'SM',
-                'minimum' => null,
-                'minimum_direction' => null,
-                'ndv' => false,
-                'remaining' => 'EEE',
-            ),
-            array(
-                'chunk' => '10SM FFF',
-                'cavok' => false,
-                'visibility' => 10,
-                'visibility_unit' => 'SM',
-                'minimum' => null,
-                'minimum_direction' => null,
-                'ndv' => false,
-                'remaining' => 'FFF',
-            ),
-            array(
-                'chunk' => '3/4SM GGG',
-                'cavok' => false,
-                'visibility' => 0.75,
-                'visibility_unit' => 'SM',
-                'minimum' => null,
-                'minimum_direction' => null,
-                'ndv' => false,
-                'remaining' => 'GGG',
-            ),
-            array(
-                'chunk' => '//// HHH',
-                'cavok' => false,
-                'visibility' => null,
-                'visibility_unit' => null,
-                'minimum' => null,
-                'minimum_direction' => null,
-                'ndv' => false,
-                'remaining' => 'HHH',
-            ),
-            array(
-                'chunk' => '9000NDV JJJ',
-                'cavok' => false,
-                'visibility' => 9000,
-                'visibility_unit' => 'm',
-                'minimum' => null,
-                'minimum_direction' => null,
-                'ndv' => true,
-                'remaining' => 'JJJ',
-            ),
-        );
-    }
+	public function getChunk()
+	{
+		return array(
+			array(
+				'chunk' => '0200 AAA',
+				'cavok' => false,
+				'visibility' => 200,
+				'visibility_unit' => 'm',
+				'minimum' => null,
+				'minimum_direction' => null,
+				'ndv' => false,
+				'remaining' => 'AAA',
+			),
+			array(
+				'chunk' => 'CAVOK BBB',
+				'cavok' => true,
+				'visibility' => null,
+				'visibility_unit' => 'm',
+				'minimum' => null,
+				'minimum_direction' => null,
+				'ndv' => false,
+				'remaining' => 'BBB',
+			),
+			array(
+				'chunk' => '8000 1200N CCC',
+				'cavok' => false,
+				'visibility' => 8000,
+				'visibility_unit' => 'm',
+				'minimum' => 1200,
+				'minimum_direction' => 'N',
+				'ndv' => false,
+				'remaining' => 'CCC',
+			),
+			array(
+				'chunk' => '2500 2200 DDD',
+				'cavok' => false,
+				'visibility' => 2500,
+				'visibility_unit' => 'm',
+				'minimum' => 2200,
+				'minimum_direction' => null,
+				'ndv' => false,
+				'remaining' => 'DDD',
+			),
+			array(
+				'chunk' => '1 1/4SM EEE',
+				'cavok' => false,
+				'visibility' => 1.25,
+				'visibility_unit' => 'SM',
+				'minimum' => null,
+				'minimum_direction' => null,
+				'ndv' => false,
+				'remaining' => 'EEE',
+			),
+			array(
+				'chunk' => '10SM FFF',
+				'cavok' => false,
+				'visibility' => 10,
+				'visibility_unit' => 'SM',
+				'minimum' => null,
+				'minimum_direction' => null,
+				'ndv' => false,
+				'remaining' => 'FFF',
+			),
+			array(
+				'chunk' => '3/4SM GGG',
+				'cavok' => false,
+				'visibility' => 0.75,
+				'visibility_unit' => 'SM',
+				'minimum' => null,
+				'minimum_direction' => null,
+				'ndv' => false,
+				'remaining' => 'GGG',
+			),
+			array(
+				'chunk' => '//// HHH',
+				'cavok' => false,
+				'visibility' => null,
+				'visibility_unit' => null,
+				'minimum' => null,
+				'minimum_direction' => null,
+				'ndv' => false,
+				'remaining' => 'HHH',
+			),
+			array(
+				'chunk' => '9000NDV JJJ',
+				'cavok' => false,
+				'visibility' => 9000,
+				'visibility_unit' => 'm',
+				'minimum' => null,
+				'minimum_direction' => null,
+				'ndv' => true,
+				'remaining' => 'JJJ',
+			),
+			array(
+				'chunk' => '20KM KKK',
+				'cavok' => false,
+				'visibility' => 20000,
+				'visibility_unit' => 'm',
+				'minimum' => null,
+				'minimum_direction' => null,
+				'ndv' => false,
+				'remaining' => 'KKK',
+			),
+			array(
+				'chunk' => '15KM LLL',
+				'cavok' => false,
+				'visibility' => 15000,
+				'visibility_unit' => 'm',
+				'minimum' => null,
+				'minimum_direction' => null,
+				'ndv' => false,
+				'remaining' => 'LLL',
+			),
+		);
+	}
 
-    public function getInvalidChunk()
-    {
-        return array(
-            array('chunk' => 'CAVO EEE'),
-            array('chunk' => 'CAVOKO EEE'),
-            array('chunk' => '123 EEE'),
-            array('chunk' => '12335 EEE'),
-            array('chunk' => '1233NVV EEE'),
-        );
-    }
+	public function getInvalidChunk()
+	{
+		return array(
+			array('chunk' => 'CAVO EEE'),
+			array('chunk' => 'CAVOKO EEE'),
+			array('chunk' => '123 EEE'),
+			array('chunk' => '12335 EEE'),
+			array('chunk' => '1233NVV EEE'),
+			array('chunk' => '23KM EEE'),
+		);
+	}
 }
